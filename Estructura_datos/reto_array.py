efaa7c1e085d9ff69f2ab241ee973153042df1f9
María García <magarcster@gmail.com>
2024-07-10 17:23:32 -0400

    creacion de la def populate part 1

--- Estructura_datos/reto_array.py
+++ Estructura_datos/reto_array.py
@@ -1,4 +1,11 @@
+import random
 class Array:
     def __init__(self, size):
         self.size: size
         self.elements= []
+
+    def populate(self, metodo="random", start=0, step=1):
+        if metodo == "random":
+            self.elements = [random.randint(0,100) for_ in range(self.size)]
+        elif metodo == "sequential":
+            self.elements = [start + step * i for i in range(self.size)]
