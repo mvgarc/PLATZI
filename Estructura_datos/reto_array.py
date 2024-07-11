2beab447dc2aa8aec005866a1b78c6ade0cddf6a
María García <magarcster@gmail.com>
2024-07-10 22:35:36 -0400

    update: Manejo de error  y Método sum_elements: Este método retorna la suma de todos los elementos en el array.

--- Estructura_datos/reto_array.py
+++ Estructura_datos/reto_array.py
@@ -6,6 +6,19 @@ class Array:
 
     def populate(self, metodo="random", start=0, step=1):
         if metodo == "random":
-            self.elements = [random.randint(0,100) for_ in range(self.size)]
+            self.elements = [random.randint(0,100) for _ in range(self.size)]
         elif metodo == "sequential":
             self.elements = [start + step * i for i in range(self.size)]
+        else:
+            raise ValueError("El Metodo a aplicar debe de ser random o secuencial")
+    def sum_elements(self):
+        return sum(self.elements)
+    
+array = Array(10)
+array.populate(metodo="random")
+print("Array que contiene los números aleatorios:", array.elements)
+print("Suma de los elementos del array:", array.sum_elements())
+
+array.populate(metodo="sequential", star=1, step=1)
+print("Array que contiene los números secuenciales:", array.elements)
+print("Suma de los elementos del array:", array.sum_elements())
