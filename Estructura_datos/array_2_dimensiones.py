96f3f948fbc01fc740538845f4a54d3eeae73242
María García <magarcster@gmail.com>
2024-07-10 22:51:17 -0400

    update

--- Estructura_datos/array_2_dimensiones.py
+++ Estructura_datos/array_2_dimensiones.py
@@ -10,3 +10,16 @@ class Grid():
     
     def get_width(self):
         return len(self.data[0])
+    
+    def __getitem__(self, index):
+        return self.data[index]
+    
+    def __str__(self):
+        result = ""
+        
+        for row in range (self.get_height()):
+            for col in range(self.get_width()):
+                result += str(self.data[row][col])
+
+            result += "\n"
+        return str(result)
