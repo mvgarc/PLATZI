2fad13fe0a7216bf76b3ad1a4dba64d2185f49b3
María García <magarcster@gmail.com>
2024-07-10 22:45:25 -0400

    creacion de def para obtener el alto y ancho data

--- Estructura_datos/array_2_dimensiones.py
+++ Estructura_datos/array_2_dimensiones.py
@@ -1,7 +1,12 @@
 from crear_array import Array
 
 class Grid():
-    def __init__(self, rows, colums, fill_value=None):
+    def __init__(self, rows, columns, fill_value=None):
         self.data = Array(rows)
         for row in range (rows):
-            self.data[row]=
+            self.data[row]= Array(columns, fill_value)
+    def get_height(self):
+        return len(self.data)
+    
+    def get_width(self):
+        return len(self.data[0])
