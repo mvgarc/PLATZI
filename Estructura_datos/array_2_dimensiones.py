d78640af98da3c77e51e11f5b8919c9b73796ea8
María García <magarcster@gmail.com>
2024-07-10 22:42:11 -0400

    crecaion de la clase Grid

---
+++ Estructura_datos/array_2_dimensiones.py
@@ -0,0 +1,7 @@
+from crear_array import Array
+
+class Grid():
+    def __init__(self, rows, colums, fill_value=None):
+        self.data = Array(rows)
+        for row in range (rows):
+            self.data[row]=
