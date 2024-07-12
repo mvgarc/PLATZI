7cdb05fb57a501382bb0517719efc19e265b41ff
María García <magarcster@gmail.com>
2024-07-12 15:59:36 -0400

    creacion del reto y sus clases

---
+++ Estructura_datos/_6_reto.py
@@ -0,0 +1,9 @@
+class Nodo:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class ListaEnlazada:
+    def __init__(self):
+        self.head = None
+        
