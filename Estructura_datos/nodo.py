ac4f0b8417355eeb7d4bee6212e85ab877ae6ccd
María García <magarcster@gmail.com>
2024-07-12 13:55:54 -0400

    update

--- Estructura_datos/nodo.py
+++ Estructura_datos/nodo.py
@@ -2,3 +2,14 @@ class Nodo():
     def __init__(self, data,next=None):
         self.data= data
         self.next= next
+
+#nodo1 = None
+nodo2= Nodo("A", None)
+nodo3= Nodo("B", nodo2)
+nodo1= Nodo("C", nodo3)
+
+print(nodo2)
+print(nodo2.data)
+print (nodo3.next)
+print (nodo3.next.data)
+print (nodo1.next.data)
