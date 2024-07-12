ae75050169a89202f4ec5ab3597cf384170d0337
María García <magarcster@gmail.com>
2024-07-12 15:43:20 -0400

    ocultamos los valores de prueba de nodo hehehe

--- Estructura_datos/_4_nodo.py
+++ Estructura_datos/_4_nodo.py
@@ -3,7 +3,7 @@ class Nodo():
         self.data= data
         self.next= next
 
-#nodo1 = None
+"""#nodo1 = None
 nodo2= Nodo("A", None)
 nodo3= Nodo("B", nodo2)
 nodo1= Nodo("C", nodo3)
@@ -12,4 +12,4 @@ print(nodo2)
 print(nodo2.data)
 print (nodo3.next)
 print (nodo3.next.data)
-print (nodo1.next.data)
+print (nodo1.next.data)"""
